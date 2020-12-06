57d1997c9ecda1121d0c25ac13f1e8c886fcb65c
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2020-12-05 19:27:05 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,6 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
  
-var particles = [];
 var plinkos = [];
 var divisions = [];
 var particle;
@@ -44,10 +43,8 @@ function setup() {
 
     
 }
- 
-
-
 function draw() {
+  Engine.update(engine);
   background("black");
   fill("yellow");
   strokeWeight(4);
@@ -68,22 +65,41 @@ function draw() {
   text("200", 680, 780);
   text("200",760,780);
   text("200", 840, 780);
-  Engine.update(engine);
- 
   
    for (var i = 0; i < plinkos.length; i++) {
      plinkos[i].display(); 
    }
-   if(frameCount%60===0){
+   /*if(frameCount%60===0){
      particles.push(new Particle(random(width/2-30, width/2+30), 10,10));
      score++;
    }
+   */
  
-  for (var j = 0; j < particles.length; j++) {
+  /*for (var j = 0; j < particles.length; j++) {
      particles[j].display();
-   }
+   }*/
    for (var k = 0; k < divisions.length; k++) {
      divisions[k].display();
+   }
+     if(particle != null){
+      particle.display();
+      if(particle.body.position.y>760){
+        if(particle.body.position.x<300){
+          score = score+500;
+          particle = null;
+          if(turn>=5) gameState = "end";
+        }
+        if(particle.body.position.x>=300&& particle.body.position.x<600){
+          score = score+100;
+          particle = null;
+          if(turn>=5) gameState = "end";
+        }
+        if(particle.body.position.x>=600&& particle.body.position.x<900){
+          score = score+200;
+          particle = null;
+          if(turn>=5) gameState = "end";
+        }
+      }
    }
 }
 
@@ -91,38 +107,5 @@ function mousePressed(){
   if(gameState!=="end"){
   turn++;
   particle = new Particle(mouseX, 10, 10, 10);
-  }
-}
-
-if(particlel = null){
-  particle.display();
-  if(particle.body.position.y>760){
-    if(particle.body.position.x<300){
-      score = score+500;
-      particle = null;
-      if(turn>=5) gameState = "end";
-    }
-  }
-}
-
-if(particlel = null){
-  particle.display();
-  if(particle.body.position.y>760){
-    if(particle.body.position.x>=300&& particle.body.position.x<600){
-      score = score+100;
-      particle = null;
-      if(turn>=5) gameState = "end";
-    }
-  }
-}
-
-if(particlel = null){
-  particle.display();
-  if(particle.body.position.y>760){
-    if(particle.body.position.x>=600&& particle.body.position.x<900){
-      score = score+200;
-      particle = null;
-      if(turn>=5) gameState = "end";
-    }
   }
 }
